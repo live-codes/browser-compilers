ec7e3c27f9e25bcd8d267553632db179821016c2
Hatem Hosny <hatemhosny@gmail.com>
2026-02-16 20:05:03 +0200

    fix dotenv

--- vendor_modules/imports/dotenv.js
+++ vendor_modules/imports/dotenv.js
@@ -1,6 +1,2 @@
-import { parse } from 'dotenv';
-import { expand } from 'dotenv-expand';
-
-export { parse, expand };
-export const parseAndExpand = (/** @type {string} */ env) =>
-  expand({ parsed: parse(env || '') }).parsed;
+export { config, parse, populate } from 'dotenv';
+export { expand } from 'dotenv-expand';
